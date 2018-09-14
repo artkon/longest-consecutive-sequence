48e48bc7b9e23f4076de511e8bebf77a5887bbe9
artkon <artemkonopelko@gmail.com>
2018-09-14 05:27:25 +0300

    implement function LongestConsecutiveSequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if(array.length == 0) return 0;
+  if(array.length == 1) return 1;
+
+  array.sort((a, b) => a - b);
+  maxSequenceCount = 1;
+  var count = 1;
+
+  for(var i = 0; i < array.length; i++){
+  	if( array[i] == array[i + 1]) continue;
+    if( (array[i] + 1) == array[i + 1] ){
+      count++;
+    } else {
+      if(count > maxSequenceCount){
+        maxSequenceCount = count;
+      }
+      count = 1;
+    }
+    
+  }
+  return maxSequenceCount;
 }

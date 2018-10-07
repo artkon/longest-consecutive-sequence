ee3324cda7f0dd90ea59afd1c3991abacb6ca231
artkon <artemkonopelko@gmail.com>
2018-10-07 15:04:45 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ module.exports = function longestConsecutiveLength(array) {
 
   array.sort((a, b) => a - b);
   maxSequenceCount = 1;
-  var count = 1;
+  let count = 1;
 
-  for(var i = 0; i < array.length; i++){
+  for(let i = 0; i < array.length; i++){
   	if( array[i] == array[i + 1]) continue;
     if( (array[i] + 1) == array[i + 1] ){
       count++;
